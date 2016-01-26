b5ad6887da7dcea8b2dc3a6dcdcd872cebf71122
ducky <richard.lin@berkeley.edu>
2016-01-26 13:59:01 -0800

    Scala-generated constant values, updated tester, always@* blocks in float black box to make bitstoreal and friends dynamic

--- src/test/resources/BlackBoxFloat.v
+++ src/test/resources/BlackBoxFloat.v
@@ -1,62 +1,16 @@
-module BBFZero(
-    input  [0:0] clock,
-    input  [0:0] reset,
-    output [63:0] io$out
-);
-  assign io$out = $realtobits(0.0);
-endmodule
-
-module BBFOne(
-    input  [0:0] clock,
-    input  [0:0] reset,
-    output [63:0] io$out
-);
-  assign io$out = $realtobits(1.0);
-endmodule
-
-module BBFTwo(
-    input  [0:0] clock,
-    input  [0:0] reset,
-    output [63:0] io$out
-);
-  assign io$out = $realtobits(2.0);
-endmodule
-
-module BBFThree(
-    input  [0:0] clock,
-    input  [0:0] reset,
-    output [63:0] io$out
-);
-  assign io$out = $realtobits(3.0);
-endmodule
-
-module BBFFour(
-    input  [0:0] clock,
-    input  [0:0] reset,
-    output [63:0] io$out
-);
-  assign io$out = $realtobits(4.0);
-endmodule
-
-module BBFSix(
-    input  [0:0] clock,
-    input  [0:0] reset,
-    output [63:0] io$out
-);
-  assign io$out = $realtobits(6.0);
-endmodule
-
 module BBFAdder(
     input  [0:0] clock,
     input  [0:0] reset,
     input  [63:0] io$in1,
     input  [63:0] io$in2,
-    output [63:0] io$out
+    output reg [63:0] io$out
 );
+  always @* begin
   real a = $bitstoreal(io$in1);
   real b = $bitstoreal(io$in2);
   real c = a + b;
-  assign io$out = $realtobits(c);
+  io$out <= $realtobits(c);
+  end
 endmodule
 
 module BBFMult(
@@ -66,8 +20,10 @@ module BBFMult(
     input  [63:0] io$in2,
     output [63:0] io$out
 );
+  always @* begin
   real a = $bitstoreal(io$in1);
   real b = $bitstoreal(io$in2);
   real c = a * b;
-  assign io$out = $realtobits(c);
+  io$out <= $realtobits(c);
+  end
 endmodule
--- src/test/scala/chiselTests/BlackBoxFloat.scala
+++ src/test/scala/chiselTests/BlackBoxFloat.scala
@@ -7,19 +7,6 @@ import org.scalatest._
 import Chisel._
 import Chisel.testers.BasicTester
 
-class BBFX extends BlackBox {
-  val io = new Bundle() {
-    val out = UInt(OUTPUT, 64)
-  }
-}
-
-class BBFZero extends BBFX
-class BBFOne extends BBFX
-class BBFTwo extends BBFX
-class BBFThree extends BBFX
-class BBFFour extends BBFX
-class BBFSix extends BBFX
-
 class BBFAdder extends BlackBox {
   val io = new Bundle() {
     val in1 = UInt(INPUT, 64)
@@ -36,46 +23,43 @@ class BBFMult extends BlackBox {
   }
 }
 
-class BlackBoxFloatTester extends BasicTester {
-  val mzero = Module(new BBFZero())
-  val zero = mzero.io.out
-  val mone = Module(new BBFOne())
-  val one = mone.io.out
-  val mtwo = Module(new BBFTwo())
-  val two = mtwo.io.out
-  val mthree = Module(new BBFThree())
-  val three = mthree.io.out
-  val mfour = Module(new BBFFour())
-  val four = mfour.io.out
-  val msix = Module(new BBFSix())
-  val six = msix.io.out
+object BBFConst {
+  def apply(value: Double): UInt = {
+    UInt(java.lang.Double.doubleToRawLongBits(value), width=64);
+  }
+}
 
-  val (cnt, done) = Counter(Bool(true), 4)
-  val accum = Reg(init=UInt(0, width=64))
+class BlackBoxFloatTester extends BasicTester {
+  val (cnt, _) = Counter(Bool(true), 4)
+  val accum = Reg(init=BBFConst(0.0))
 
   val adder = Module(new BBFAdder())
   adder.io.in1 := accum
-  adder.io.in2 := one
+  adder.io.in2 := BBFConst(1.0)
 
   val mult = Module(new BBFMult())
   mult.io.in1 := adder.io.out
-  mult.io.in2 := two
+  mult.io.in2 := BBFConst(2.0)
 
   accum := adder.io.out
 
-  printf("%x    a: %x + %x => %x    m: %x * %x => %x",
+  printf("%x    add: %x + %x => %x    mult: %x * %x => %x\n",
       accum,
       adder.io.in1, adder.io.in2, adder.io.out,
       mult.io.in1, mult.io.in2, mult.io.out)
 
   when (cnt === UInt(0)) {
-    assert(adder.io.out === one)
-    assert(mult.io.out === two)
+    assert(adder.io.out === BBFConst(1))
+    assert(mult.io.out === BBFConst(2))
   } .elsewhen (cnt === UInt(1)) {
-    assert(adder.io.out === two)
-    assert(mult.io.out === four)
-  }
-  when (done) {
+    assert(adder.io.out === BBFConst(2))
+    assert(mult.io.out === BBFConst(4))
+  } .elsewhen (cnt === UInt(2)) {
+    assert(adder.io.out === BBFConst(3))
+    assert(mult.io.out === BBFConst(6))
+  } .elsewhen (cnt === UInt(3)) {
+    assert(adder.io.out === BBFConst(4))
+    assert(mult.io.out === BBFConst(8))
     stop()
   }
 }
